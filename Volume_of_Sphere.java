b1f3d14b218aadb5027932d8350af3ef228e92ef
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-01-30 21:09:15 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner read = new Scanner(System.in);         float a = read.nextFloat();         System.out.printf("%.2f",(4.0/3.0)*(3.14*a*a*a));     } }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner read = new Scanner(System.in);
+        float a = read.nextFloat();
+        System.out.printf("%.2f",(4.0/3.0)*(3.14*a*a*a));
+    }
+}
