20c5669e65d6800f485a24eb0f98712711166630
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-02-01 10:54:25 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner read = new Scanner(System.in);         int a = read.nextInt();         if (a>98) System.out.println("YES");         else System.out.println("NO");     } }

---
+++ Fever.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner read = new Scanner(System.in);
+        int a = read.nextInt();
+        if (a>98) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
