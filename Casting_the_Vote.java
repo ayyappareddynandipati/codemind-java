922722bde8fe453bcccc4f318a6b8abe15aadc98
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-02-09 20:51:45 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner read = new Scanner(System.in);         int a = read.nextInt();         if (a>17) System.out.print("YES");         else System.out.print("NO");     } }

---
+++ Casting_the_Vote.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner read = new Scanner(System.in);
+        int a = read.nextInt();
+        if (a>17) System.out.print("YES");
+        else System.out.print("NO");
+    }
+}
