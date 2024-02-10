3079cc6d5b7c4452fc4c7d0ce94a0d7d5c27a219
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-02-10 21:25:32 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner read = new Scanner(System.in);         String s = read.nextLine();         System.out.println(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner read = new Scanner(System.in);
+        String s = read.nextLine();
+        System.out.println(s);
+    }
+}
