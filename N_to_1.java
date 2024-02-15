3b91e3e062c9d1ca7f4a5d070141b006bae76cd8
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-02-15 21:32:18 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner read = new Scanner(System.in);         int n = read.nextInt();         for(int i=n;i>0;i--)         {             System.out.print(i + " ");         }     } }

---
+++ N_to_1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner read = new Scanner(System.in);
+        int n = read.nextInt();
+        for(int i=n;i>0;i--)
+        {
+            System.out.print(i + " ");
+        }
+    }
+}
