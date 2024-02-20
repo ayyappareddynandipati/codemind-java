cc45252f3f9ccdbf2562c70753d8034c3906b8ce
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-02-20 21:38:01 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         if (a%2==0) System.out.print(2);         else System.out.print(1);     } }

---
+++ IsEven?.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a%2==0) System.out.print(2);
+        else System.out.print(1);
+    }
+}
