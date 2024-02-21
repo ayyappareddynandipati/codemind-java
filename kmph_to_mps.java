95b90047c7b8082300540427dfcf6efbab319a3c
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-02-21 22:06:23 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner read = new Scanner(System.in);         float a = read.nextFloat();         System.out.printf("%.2f",a*0.277778);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner read = new Scanner(System.in);
+        float a = read.nextFloat();
+        System.out.printf("%.2f",a*0.277778);
+    }
+}
