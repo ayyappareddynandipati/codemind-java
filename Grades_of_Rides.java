9018912b31838e51dc218de2dd33695fe28bbeb9
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-02-22 18:49:49 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         int c = sc.nextInt();         if (a>50 && b>60 && c>100) System.out.print(10);         else if (a>50 && b>60) System.out.print(9);         else if (b>60 && c>100) System.out.print(8);         else if (a>50 && c>100) System.out.print(7);         else if (a>50 || b>60 || c>100) System.out.print(6);         else System.out.print(5);     } }

--- Grades_of_Rides.java
+++ Grades_of_Rides.java
@@ -3,10 +3,10 @@ public class MyClass
 {
     public static void main(String args[])
     {
-        Scanner read = new Scanner(System.in);
-        int a = read.nextInt();
-        int b = read.nextInt();
-        int c = read.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
         if (a>50 && b>60 && c>100) System.out.print(10);
         else if (a>50 && b>60) System.out.print(9);
         else if (b>60 && c>100) System.out.print(8);
