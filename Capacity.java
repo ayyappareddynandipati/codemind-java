8faf7f95c94939a1565776fd56405085ddcfb32f
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-02-24 19:00:18 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner read = new Scanner(System.in);         int a = read.nextInt();         int b = read.nextInt();         int c = read.nextInt();         System.out.println(a*b*c + " KB");     } }

---
+++ Capacity.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner read = new Scanner(System.in);
+        int a = read.nextInt();
+        int b = read.nextInt();
+        int c = read.nextInt();
+        System.out.println(a*b*c + " KB");
+    }
+}
