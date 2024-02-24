7f51640ca97511c77e9ee3a3b032a7ae1ac4e04b
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-02-24 18:58:58 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner read = new Scanner(System.in);         int a = read.nextInt();         System.out.printf("%.2f",a*2.54);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner read = new Scanner(System.in);
+        int a = read.nextInt();
+        System.out.printf("%.2f",a*2.54);
+    }
+}
