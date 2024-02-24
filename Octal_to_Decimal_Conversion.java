5c83566a31cc60a798ba05c6b4a162e9af564189
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-02-24 18:56:57 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner read = new Scanner(System.in);         String a = read.nextLine();         System.out.println(Integer.parseInt(a,8));     } }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner read = new Scanner(System.in);
+        String a = read.nextLine();
+        System.out.println(Integer.parseInt(a,8));
+    }
+}
