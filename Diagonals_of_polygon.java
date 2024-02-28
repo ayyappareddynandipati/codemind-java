62d5933bc4f48e90c452f36e343e849b67c9f385
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-02-28 23:09:20 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         System.out.print(n*(n-3)/2);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(n*(n-3)/2);
+    }
+}
