f22d5461874cb59b1da357aa5cdf76ff8dd3da01
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-02-29 22:43:07 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int p = sc.nextInt();         int t = sc.nextInt();         int r = sc.nextInt();         System.out.print(p*t*r/100);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        int t = sc.nextInt();
+        int r = sc.nextInt();
+        System.out.print(p*t*r/100);
+    }
+}
