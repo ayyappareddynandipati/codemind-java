861ba9d7fce39768c454a257d832b9ac3a09900b
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-03-14 21:19:41 +0530

    import java.util.*;
    public class MyClass {
        public static void main(String[] args) {
            Scanner scanner = new Scanner(System.in);
            int n = scanner.nextInt();
            int[] arr = new int[n];
            for (int i = 0; i < n; i++) {
                arr[i] = scanner.nextInt();
            }
    
            int sum = 0;
            for (int i = 0; i < n; i++) {
                if (arr[i] % 2 != 0) {
                    break; 
                }
                sum += arr[i];
            }
            System.out.println(sum);
        }
    }

---
+++ sum_of_array__elements_upto__first_odd_number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class MyClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 != 0) {
+                break; 
+            }
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
