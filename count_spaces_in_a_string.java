f3685dfc65bdea867d55306a395fe90de2ce4d07
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-03-16 09:22:55 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         String s = sc.nextLine();         int cnt=0,n=s.length();         for(int i=0;i<n;i++)         {             if (s.charAt(i)==' ')             {                 cnt++;             }         }         System.out.print(cnt);     } }

---
+++ count_spaces_in_a_string.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int cnt=0,n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            if (s.charAt(i)==' ')
+            {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
